dbbcd030372f57cbb49eacf9991fae9b70dae53a
Hrudhayasankalp <sankalphrudhaya@gmail.com>
2026-06-04 10:43:58 +0530

    Time: 2 ms (3.74%) | Memory: 46.8 MB (11.58%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,32 +1,8 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int l=0;
-        int r=nums.length-1;
-        while (l<=r) {
-            int mid=(l+r)/2;
-            
-            if(nums[mid]==target){
-                return true;
-            }
-            if(nums[mid]==nums[l]){
-                l++;
-                continue;
-            }
-            if(nums[l]<=nums[mid]) {
-                if (nums[l]<=target && target<nums[mid]) {
-                    r=mid-1;
-                } else {
-                    l=mid+1;
-                }
-            } else{
-                if(nums[mid]<target && target<=nums[r]) {
-                    l=mid+1;
-                } else {
-                    r=mid-1;
-                }
-            }
-        }
-        
-        return false;        
+       HashSet<Integer> s=new HashSet<>();
+       for(int i:nums)s.add(i);
+       if(s.contains(target)) return true;
+       else return false;  
     }
 }
