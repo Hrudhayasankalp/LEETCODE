631a6a5dc1f9c1d7f0aa3b3e7337c59867419599
Hrudhayasankalp <sankalphrudhaya@gmail.com>
2026-06-16 11:20:21 +0530

    Time: 4 ms (76.47%) | Memory: 55.3 MB (48.53%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String processStr(String s) {
+        int i=0;
+        StringBuilder h=new StringBuilder();
+        while(i<s.length()){
+            if(s.charAt(i)=='*'){
+                if(h.length()>0)
+                    h.deleteCharAt(h.length()-1);
+            }
+            else if(s.charAt(i)=='#')h.append(h.toString());
+            else if(s.charAt(i)=='%')h.reverse();
+            else h.append(s.charAt(i));
+            i++;
+        }
+        return h.toString();
+    }
+}
